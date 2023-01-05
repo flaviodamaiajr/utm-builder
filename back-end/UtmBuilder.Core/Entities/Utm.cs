0d0e046f6fb6eee6d68f967b333fc6a0f85ece1e
Flávio A. da Maia Jr <flaviodamaiajr@gmail.com>
2023-01-04 21:38:14 -0300

    feat: Create business core and unit tests.

--- back-end/UtmBuilder.Core/Entities/Utm.cs
+++ back-end/UtmBuilder.Core/Entities/Utm.cs
@@ -1,16 +1,68 @@
+using UtmBuilder.Core.Extensions;
 using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
 
-namespace UtmBuilder.Core.Entities
+namespace UtmBuilder.Core;
+
+public class Utm
 {
-    public class Utm
+    public Utm(Url url, Campaign campaign)
     {
-        public Utm(Url url, Campaign campaign)
-        {
-            Url = url;
-            Campaign = campaign;
+        Url = url;
+        Campaign = campaign;
+    }
+    
+    /// <summary>
+    /// URL (Website Link)
+    /// </summary>
+    public Url Url { get; }
+    
+    /// <summary>
+    /// Campaign Details
+    /// </summary>
+    public Campaign Campaign { get; }
 
-        }
-        public Url Url { get; }
-        public Campaign Campaign { get; }
+    public static implicit operator string(Utm utm) 
+        => utm.ToString();
+
+    public static implicit operator Utm(string link)
+    {
+        if (string.IsNullOrEmpty(link))
+            throw new InvalidUrlException();
+
+        var url = new Url(link);
+        var segments = url.Address.Split("?");
+        if (segments.Length == 1)
+            throw new InvalidUrlException("No segments were provided");
+
+        var pars = segments[1].Split("&");
+        var source = GetSource(pars, "utm_source");
+        var medium = GetSource(pars, "utm_medium");
+        var name = GetSource(pars, "utm_name");
+        var id = GetSource(pars, "utm_id");
+        var term = GetSource(pars, "utm_term");
+        var content = GetSource(pars, "utm_content");
+
+        var utm = new Utm(
+            new Url(segments[0]),
+            new Campaign(source, medium, name, id, term, content));
+        return utm;
+    }
+
+    private static string GetSource(string[] pars, string parameter)
+    {
+        return pars.Where(x => x.StartsWith(parameter)).FirstOrDefault("").Split("=")[1];
+    }
+
+    public override string ToString()
+    {
+        var segments = new List<string>();
+        segments.AddIfNotNull("utm_source", Campaign.Source);
+        segments.AddIfNotNull("utm_medium", Campaign.Medium);
+        segments.AddIfNotNull("utm_campaign", Campaign.Name);
+        segments.AddIfNotNull("utm_id", Campaign.Id);
+        segments.AddIfNotNull("utm_term", Campaign.Term);
+        segments.AddIfNotNull("utm_content", Campaign.Content);
+        return $"{Url.Address}?{string.Join("&", segments)}";
     }
 }
